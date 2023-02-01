68c3b3fcb220fd27135d25d273eecca580d634a6
Casey Woodman <caseytwoodman@gmail.com>
2023-01-31 21:54:24 -0600

    added user interface

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 
 const postRoutes = require("./post-routes.js");
+const userRoutes = require("./user-routes");
 
+router.use("/users", userRoutes);
 router.use("/post", postRoutes);
 
 module.exports = router;
