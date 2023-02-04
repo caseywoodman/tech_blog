6cd4a516ba7dab4255d65ad06fff60c57ded8ddf
Casey Woodman <caseytwoodman@gmail.com>
2023-02-04 10:20:33 -0600

    added create a post function

--- public/js/createpost.js
+++ public/js/createpost.js
@@ -1,3 +1,18 @@
-document.querySelector("#createPost").addEventListener("click", createPost);
+async function createPost(event) {
+  const title = document.querySelector("#title").value.trim();
+  const body = document.querySelector("#body").value.trim();
+  const response = await fetch("/api/post", {
+    method: "POST",
+    body: JSON.stringify({ title, body }),
+    headers: { "Content-Type": "application/json" },
+  });
+  if (response.ok) {
+    console.log("good response");
+    document.location.replace("/dashboard");
+  } else {
+    alert("Post Failed");
+  }
+  console.log(response);
+}
 
-createPost;
+document.querySelector("#createPost").addEventListener("click", createPost);
