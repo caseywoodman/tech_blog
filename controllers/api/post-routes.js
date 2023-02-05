302db3c113cebea2c49ebdb4652f93d6b858cf70
Casey Woodman <caseytwoodman@gmail.com>
2023-02-05 08:40:39 -0600

    finalized delete a post function

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -33,4 +33,21 @@ router.put("/:id", async (req, res) => {
   }
 });
 
+router.delete("/:id", async (req, res) => {
+  try {
+    const postData = await Post.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+    if (!postData) {
+      res.status(404).json({ message: "NO POST WITH THIS ID" });
+      return;
+    }
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 module.exports = router;
--- public/js/updatepost.js
+++ public/js/updatepost.js
@@ -1,20 +1,30 @@
+const id = document.querySelector("#postId").getAttribute("data-id");
+
 async function updatePost(event) {
   const title = document.querySelector("#title").value.trim();
   const body = document.querySelector("#body").value;
-  const id = document.querySelector("#postId").getAttribute("data-id");
-  console.log(title, body, id);
   const response = await fetch(`/api/post/${id}`, {
     method: "PUT",
     body: JSON.stringify({ title, body }),
     headers: { "Content-Type": "application/json" },
   });
   if (response.ok) {
-    console.log("good response");
     document.location.replace("/dashboard");
   } else {
     alert("Update Failed");
   }
-  console.log(response, "test");
+}
+
+async function deletePost(event) {
+  const response = await fetch(`/api/post/${id}`, {
+    method: "DELETE",
+  });
+  if (response.ok) {
+    document.location.replace("/dashboard");
+  } else {
+    alert("DELETE FAILED");
+  }
 }
 
 document.querySelector("#savePost").addEventListener("click", updatePost);
+document.querySelector("#deletePost").addEventListener("click", deletePost);
